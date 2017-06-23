e1ca5320d115752257d2a1422c64642be0ca6719
Alex W <wapniarski@gmail.com>
2017-06-23 15:18:57 -0700

    jsx

--- notes.md
+++ notes.md
@@ -6,9 +6,12 @@ cd my-app
 npm start
 ```
 
+- command+P - search
 - command+shift+D - debugger
 - create `launch.json` debugger file
 - set enviroment to chrome
 - start debugger with play button
 - refresh F5 to hit breakpoint
 - F10 to step through
+- command+shift+P - control palette
+- create eslintrc.json
--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-var element = React.createElement('h1', {className: 'greeting'}, 'Hello, world!');
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Alex',
+  lastName: 'Wap'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
+// var element = React.createElement('h1', {className: 'greeting'}, 'Hello, world!');
 ReactDOM.render(element, document.getElementById('root'));
 registerServiceWorker();
+
